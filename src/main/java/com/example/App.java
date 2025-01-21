437e0fa7bd40919c175215475dfe85d580e1011f
Eldaon <97115162+Eldaon@users.noreply.github.com>
2025-01-21 11:40:45 +0100

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.nio.ByteBuffer;
 
 import com.example.Controller.Controller;
-import com.example.Model.Document;
-import com.example.Model.LineModel;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -14,25 +12,19 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
-
     @Override
     public void start(Stage stage) throws Exception {
         Controller ctrl = new Controller();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("View/FolderView.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("View/NameView.fxml"));
         byte[] byteArray = ByteBuffer.allocate(2).putShort((short) 200).array();
-
+        
         Scene scene = new Scene(loader.load());
-        stage.setHeight(400);
-        stage.setWidth(500);
-        stage.setTitle("Éditeur partagé");
-
+        stage.setTitle("CoEdit - Éditeur partagé");
         File iconFile = new File("src/main/resources/icons/icons_coedit.png");
         stage.getIcons().add(new Image(iconFile.toURI().toString()));
-
         stage.setScene(scene);
         stage.show();
     }
-
    
     public static void main(String[] args) {
         launch();
